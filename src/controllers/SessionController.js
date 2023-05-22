e92a2dfb3237eb235af5f7a659549641772cfa55
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-22 20:09:43 -0300

    cria um usuario no db

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,28 @@
+//metodos
+/*index, show, store, update, destroy
+index: listagem de sessoes
+store: criar uma sessao
+show: listar uma unica sessao
+update: atualizar/editar uma sessao
+destroy: deletar uma sessao
+*/
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email } = req.body;
+    //chama o metodo create do model User e passa o email como parametro criando um novo usuario
+    // let user = await User.create({ email });
+
+    //verifica se o usuario ja existe
+    let user = await User.findOne({ email });
+    if (!user) {
+      //cria um novo usuario
+      user = await User.create({ email });
+    }
+    return res.json(user);
+  }
+}
+
+export default new SessionController();
