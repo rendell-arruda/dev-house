4812f71ab6020898c40bf2a5ad6341706340970d
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-23 19:10:51 -0300

    cadastrando multipart

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import SessionController from './controllers/SessionController';
+import HouseController from './controllers/HouseController';
+import multer from 'multer';
+import uploudConfig from './config/uploud';
 
 const routes = new Router();
+const uploud = multer(uploudConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.post('/houses', uploud.single('thumbnail'), HouseController.store);
 
 export default routes;
