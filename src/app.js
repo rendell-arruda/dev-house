12317be35829216d49dea14f7e8913023958d226
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-25 19:49:45 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
@@ -17,6 +18,8 @@ class App {
     this.routes();
   }
   middlewares() {
+    this.server.use(cors());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'uploads'))
