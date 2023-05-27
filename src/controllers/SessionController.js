fb5bf699795a207eb2a8dc74095cf9d004647cd4
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-27 17:22:24 -0300

    validacao de email

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -8,13 +8,21 @@ destroy: deletar uma sessao
 */
 
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required()
+    });
+
     const { email } = req.body;
     //chama o metodo create do model User e passa o email como parametro criando um novo usuario
     // let user = await User.create({ email });
 
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
     //verifica se o usuario ja existe
     let user = await User.findOne({ email });
     if (!user) {
